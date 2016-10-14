5dfbe24e21c9b69a353c72128b068fbfc8749b5a
Martin Bielik <martin.bielik@outlook.sk>
2016-10-15 01:19:42 +0200

    show/hide/cancel/save options

--- src/components/UserOptions.js
+++ src/components/UserOptions.js
@@ -3,41 +3,128 @@ import { View, Text, TouchableHighlight } from 'react-native'
 
 const mapObject = (obj, fn) => Object.keys(obj).map(k => fn([ k, obj[k] ]))
 
-
-export class UserOptions extends Component {
-
-  constructor(...props) {
-    super(...props)
-  }
-
-  render() {
-    const { options } = this.props
-    const summary = mapObject(options, ([ key, value ]) => {
-      const text = `${value.name}: ${value.defaultValue}`
-      return (
-        <Text key={key}>{text}</Text>
-      )
-    })
-    return (
-      <View>
-        {summary}
-        <Button>Change</Button>
-      </View>
-    )
-  }
-
-}
-
 export const Button = ({ children, onPress }) => (
   <TouchableHighlight onPress={onPress}>
     <Text>{children}</Text>
   </TouchableHighlight>
 )
 
+export const InputText = ({ children  }) => (
+  <Text>TBD</Text>
+)
+
+const inputs = {
+  number: InputText,
+  text: InputText,
+}
+
+const makeOptions = options => Object.keys(options).reduce((r, key) => {
+  return Object.assign(
+    r, {
+      [key]: {
+        ...options[key],
+        value: options[key].defaultValue,
+      },
+    }
+  )
+}, {})
+
+export class UserOptions extends Component {
+
+  constructor(...props) {
+    super(...props)
+    this.change = this.change.bind(this)
+    this.cancel = this.cancel.bind(this)
+    this.save = this.save.bind(this)
+    this.state = {
+      editMode: false,
+      options: makeOptions(this.props.options),
+    }
+  }
+
+  render() {
+    const { editMode } = this.state
+    const summary = this.makeSummary()
+    return (
+      <View>
+        {summary}
+        <Button onPress={this.change}>Change</Button>
+        {editMode && (
+          <View>
+            {this.makeInputs()}
+            <Button onPress={this.cancel}>Cancel</Button>
+            <Button onPress={this.save}>Save</Button>
+          </View>
+        )}
+      </View>
+    )
+  }
+
+  change() {
+    this.setState({
+      editMode: true,
+      editOptions: this.state.options,
+    })
+  }
+
+  cancel() {
+    this.setState({
+      editMode: false,
+      editOptions: null,
+    })
+  }
+
+  save() {
+    this.props.onChange(this.state.editOptions)
+    this.setState({
+      editMode: false,
+      editOptions: null,
+      options: this.state.editOptions,
+    })
+  }
+
+  onInputChange(key, value) {
+    const { editOptions } = this.state
+    this.setState({
+      editOptions: {
+        ...editOptions,
+        [key]: { ...editOptions[key], value },
+      },
+    })
+  }
+
+  makeInputs() {
+    return mapObject(this.state.options, ([ key, obj ]) => {
+      const Input = inputs[obj.type]
+      const onInputChange = this.onInputChange.bind(this, key)
+      return (
+        <Input
+          key={key}
+          value={obj.value}
+          label={obj.label}
+          onChange={onInputChange}
+        />
+      )
+    })
+  }
+
+  makeSummary() {
+    return mapObject(this.state.options, ([ key, obj ]) => {
+      const text = `${obj.label}: ${obj.value}`
+      return (
+        <Text key={key}>{text}</Text>
+      )
+    })
+  }
+
+}
+
 UserOptions.propTypes = {
   options: PropTypes.object,
+  onChange: PropTypes.func,
 }
 
 UserOptions.defaultProps = {
   options: {},
+  onChange: () => 0,
 }
--- src/components/UserOptions.spec.js
+++ src/components/UserOptions.spec.js
@@ -4,8 +4,6 @@ import { shallow } from 'enzyme'
 import { UserOptions } from './UserOptions'
 import { View } from 'react-native'
 
-const equalsText = text => w => w.text() === text
-
 describe('<UserOptions />', () => {
 
   it('should render view', () => {
@@ -17,23 +15,99 @@ describe('<UserOptions />', () => {
     const options = {
       number: {
         type: 'number',
-        name: 'Number',
+        label: 'Number',
         defaultValue: 1,
       },
       myText: {
         type: 'text',
-        name: 'My text',
+        label: 'My text',
         defaultValue: 'Hello world',
       },
     }
     const wrapper = shallow(<UserOptions options={options} />)
-    expect(wrapper.findWhere(equalsText('Number: 1')).length).toBe(1)
-    expect(wrapper.findWhere(equalsText('My text: Hello world')).length).toBe(1)
+    expect(wrapper.find({ children: 'Number: 1' }).length).toBe(1)
+    expect(wrapper.find({ children: 'My text: Hello world' }).length).toBe(1)
   })
 
   it('should render change button', () => {
     const wrapper = shallow(<UserOptions />)
-    expect(wrapper.findWhere(equalsText('Change')).length).toBe(1)
+    expect(wrapper.find('Button[children="Change"]').length).toBe(1)
+  })
+
+  it('should display edit mode', () => {
+    const options = {
+      number: {
+        type: 'number',
+        label: 'Number',
+        defaultValue: 1,
+      },
+    }
+    const wrapper = shallow(<UserOptions options={options} />)
+    const change = wrapper.find('Button[children="Change"]')
+    expect(wrapper.find('Button[children="Cancel"]').length).toBe(0)
+    expect(wrapper.find('Button[children="Save"]').length).toBe(0)
+    expect(wrapper.find('InputText[label="Number"]').length).toBe(0)
+    change.simulate('press')
+    wrapper.update()
+    expect(wrapper.find('Button[children="Cancel"]').length).toBe(1)
+    expect(wrapper.find('Button[children="Save"]').length).toBe(1)
+    expect(wrapper.find('InputText[label="Number"]').length).toBe(1)
+  })
+
+  it('should cancel editing', () => {
+    const options = {
+      number: {
+        type: 'number',
+        label: 'Number',
+        defaultValue: 1,
+      },
+    }
+    const wrapper = shallow(<UserOptions options={options} />)
+    const change = wrapper.find('Button[children="Change"]')
+    change.simulate('press')
+    wrapper.update()
+    const input = wrapper.find('InputText[label="Number"]')
+    input.simulate('change', 3)
+    const close = wrapper.find('Button[children="Cancel"]')
+    close.simulate('press')
+    wrapper.update()
+    expect(wrapper.find('Button[children="Cancel"]').length).toBe(0)
+    expect(wrapper.find('Button[children="Save"]').length).toBe(0)
+    expect(wrapper.find('InputText[key="Number"]').length).toBe(0)
+    expect(wrapper.find('Button[children="Change"]').length).toBe(1)
+    expect(wrapper.find({ children: 'Number: 1' }).length).toBe(1)
+  })
+
+  it('should edit and trigger onChange event', () => {
+    const options = {
+      number: {
+        type: 'number',
+        label: 'Number',
+        defaultValue: 1,
+      },
+    }
+    const onChange = jest.fn()
+    const wrapper = shallow(<UserOptions options={options} onChange={onChange} />)
+    const change = wrapper.find('Button[children="Change"]')
+    change.simulate('press')
+    wrapper.update()
+    const input = wrapper.find('InputText[label="Number"]')
+    input.simulate('change', 3)
+    wrapper.update()
+    const save = wrapper.find('Button[children="Save"]')
+    save.simulate('press')
+    wrapper.update()
+    expect(wrapper.find({ children: 'Number: 1' }).length).toBe(0)
+    expect(wrapper.find({ children: 'Number: 3' }).length).toBe(1)
+    expect(onChange.mock.calls.length).toBe(1)
+    expect(onChange.mock.calls[0][0]).toEqual({
+      number: {
+        type: 'number',
+        label: 'Number',
+        defaultValue: 1,
+        value: 3,
+      },
+    })
   })
 
 })
