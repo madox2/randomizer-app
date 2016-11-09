a07696156a2409d749eda5caf596ac3ed88651c9
Martin Bielik <martin.bielik@outlook.sk>
2016-11-09 18:49:53 +0100

    refined coin animation

--- src/screens/Coin.js
+++ src/screens/Coin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { TouchableOpacity, View, StyleSheet, Animated } from 'react-native'
+import { TouchableOpacity, StyleSheet, Animated, Easing, Image } from 'react-native'
 import { SectionTemplate } from '../components/SectionTemplate'
 import { randomBoolean } from '../utils/random'
 
@@ -10,30 +10,47 @@ export class Coin extends Component {
     this.state = {
       side: randomBoolean(),
       animationIdx: 0,
-      anim: new Animated.Value(0),
+      rotation: new Animated.Value(0),
     }
     this.throwCoin = this.throwCoin.bind(this)
   }
 
   throwCoin() {
+    if (this.state.rotating) {
+      return
+    }
+    const time = new Animated.Value(0)
+    Animated.timing(time, {
+      toValue: 20,
+      duration: 1000,
+      easing: Easing.linear,
+    }).start(() => {
+      // FIXME: this._parent.removeListener is not a function
+      // it fails to set state for the first time and animated value
+      // must be set to prevent from next fails. is it web related only?
+      // quickfix: set animated value and catch error for the first setState
+      try {
+        this.setState({
+          rotation: new Animated.Value(0),
+        })
+      } catch(e) {
+        console.warn(e)
+      }
+      this.setState({
+        rotation: new Animated.Value(0),
+        rotating: false,
+        side: randomBoolean(),
+      })
+    })
     this.setState({
-      anim: new Animated.Value(0),
-      side: randomBoolean(),
-    }, () => {
-      Animated.timing(
-        this.state.anim,
-        {toValue: this.size}
-      ).start()
+      rotation: Animated.modulo(time, 2),
+      rotating: true,
     })
   }
 
-  componentWillMount() {
-    this.size = 20
-    this.inputRange = new Array(this.size).fill(0).map((a, i) => i)
-    this.outputRange = new Array(this.size).fill(0).map((a, i) => (i + 1) % 2)
-  }
-
-  componentDidMount() {
+  makeUri() {
+    const idx = this.state.side ? 0 : 1
+    return `../resources/images/coin${idx}.svg`
   }
 
   render() {
@@ -43,26 +60,29 @@ export class Coin extends Component {
         color={this.props.color}
       >
         <TouchableOpacity
-          style={s.touchable}
           onPress={this.throwCoin}
           activeOpacity={0.6}
+        style={s.touchable}
         >
-          <View style={{ width: 230, height: 230 }}>
-              <Animated.Image
-                source={{ uri: '../resources/images/coin.svg' }}
-                style={{
-                  height: 230,
-                  width: 230,
-                  transform: [
-                    {scaleX: this.state.anim.interpolate({
-                      inputRange: this.inputRange,
-                      outputRange: this.outputRange,
-                    })},
-                    {rotate: this.state.side ? '0deg' : '180deg'},
-                  ],
-                }}
-              />
-          </View>
+          {!this.state.rotating &&
+            <Image
+              source={{ uri: this.makeUri()}}
+              style={s.image}
+            />
+          }
+          {this.state.rotating &&
+            <Animated.Image
+              source={{ uri: '../resources/images/coin2.svg' }}
+              style={[s.image, {
+                transform: [
+                  {scaleY: this.state.rotation.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [1, 0],
+                  })},
+                ],
+              }]}
+            />
+          }
         </TouchableOpacity>
       </SectionTemplate>
     )
@@ -75,5 +95,9 @@ const s = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 230,
+    height: 230,
   },
 })
