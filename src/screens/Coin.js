f911bc492fdf790ecd6741de20b7c71296fa58e3
Martin Bielik <martin.bielik@outlook.sk>
2016-11-20 12:17:03 +0100

    coin refactoring, still buggy

--- src/screens/Coin.js
+++ src/screens/Coin.js
@@ -7,39 +7,18 @@ export class Coin extends Component {
 
   constructor(...args) {
     super(...args)
-    this.state = {
-      animationIdx: 0,
-      rotation: new Animated.Value(0),
-    }
     this.throwCoin = this.throwCoin.bind(this)
+    this.time = new Animated.Value(0)
+    this.rotation = Animated.modulo(this.time, 2)
   }
 
   throwCoin() {
-    if (this.state.rotating) {
-      return
-    }
-    const time = new Animated.Value(0)
-    Animated.timing(time, {
+    this.time.setValue(0)
+    Animated.timing(this.time, {
       toValue: 15 + randomNumber(0, 1),
       duration: 1000,
       easing: Easing.linear,
-    }).start(() => {
-      // FIXME: this._parent.removeListener is not a function
-      // it fails to set state for the first time and animated value
-      // must be set to prevent from next fails. is it web related only?
-      // quickfix: set animated value and catch error for the first setState
-      // quickfix is needed for each animated element in view
-      try { this.setState({ rotation: new Animated.Value(0) }) } catch(e) { console.warn(e) }
-      try { this.setState({ rotation: new Animated.Value(0) }) } catch(e) { console.warn(e) }
-      this.setState({
-        rotation: new Animated.Value(0),
-        rotating: false,
-      })
-    })
-    this.setState({
-      rotation: Animated.modulo(time, 2),
-      rotating: true,
-    })
+    }).start()
   }
 
   render() {
@@ -55,10 +34,11 @@ export class Coin extends Component {
         >
           <View style={{position:'relative'}}>
             <Animated.Image
+              ref={r => this.image1 = r}
               source={{ uri: '../resources/images/coin0.svg' }}
               style={[s.image, {
                 transform: [
-                  {rotateX: this.state.rotation.interpolate({
+                  {rotateX: this.rotation.interpolate({
                     inputRange: [0, 1],
                     outputRange: ['0deg', '180deg'],
                   })},
@@ -72,7 +52,7 @@ export class Coin extends Component {
               source={{ uri: '../resources/images/coin1.svg' }}
               style={[s.image, {
                 transform: [
-                  {rotateX: this.state.rotation.interpolate({
+                  {rotateX: this.rotation.interpolate({
                     inputRange: [0, 1],
                     outputRange: ['180deg', '0deg'],
                   })},
