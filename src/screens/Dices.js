088fcdadb82702750a6ef9896080512ad788cfaf
Martin Bielik <martin.bielik@outlook.sk>
2016-11-26 17:44:01 +0100

    basic dice graphic

--- src/screens/Dices.js
+++ src/screens/Dices.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, View, TouchableOpacity, Image } from 'react-native'
 import { randomNumber } from '../utils/random'
 import { SectionTemplate } from '../components/SectionTemplate'
+import { ResponsiveStyleSheet } from 'react-native-responsive-stylesheet'
 
+// TODO: implement multi sided dices
 const options = {
   count: {
     type: 'number',
     label: 'Count',
-    defaultValue: 1,
+    defaultValue: 4,
     constraints: { min: 1, max: 12 },
   },
 }
@@ -18,17 +20,24 @@ export class Dices extends Component {
     super(...args)
     this.state = {
       count: options.count.defaultValue,
-      results: [],
+      results: this.generate(options.count.defaultValue),
     }
     this.onOptionsChange = this.onOptionsChange.bind(this)
-    this.onRefresh = this.onRefresh.bind(this)
+    this.throwDices = this.throwDices.bind(this)
+    this.thrownNumber = 0
   }
 
-  onRefresh() {
-    const results = new Array(this.state.count)
+  generate(count) {
+    return new Array(count)
       .fill(0)
       .map(() => randomNumber(1, 6))
-    this.setState({ results })
+  }
+
+  throwDices() {
+    this.setState({
+      results: this.generate(this.state.count),
+      thrownNumber: this.state.throwNumber + 1,
+    })
   }
 
   onOptionsChange({ count }) {
@@ -38,18 +47,64 @@ export class Dices extends Component {
   }
 
   render() {
+    const { throwNumber, results } = this.state
+    const s = makeStyles()
     return (
       <SectionTemplate
         color={this.props.color}
-        onRefresh={this.onRefresh}
         options={options}
         onOptionsChange={this.onOptionsChange}
         title='Dices'
       >
-        <Text>{JSON.stringify(this.state.results)}</Text>
+        <TouchableOpacity
+          onPress={this.throwDices}
+          activeOpacity={0.6}
+          style={s.container}
+        >
+          {results.map((r, i) => (
+            <DiceGraphic result={r} key={`${throwNumber}-${i}`} />
+          ))}
+        </TouchableOpacity>
       </SectionTemplate>
     )
   }
 
 }
 
+const diceSource = num => ({ uri: `../resources/images/dice${num}.svg` })
+
+const DiceGraphic = ({ result }) => {
+  // TODO: responsiveness
+  const s = makeStyles()
+  if (result > 6 || result < 0) {
+    // not implemented yet
+    return (
+      <View style={[s.diceImage, s.diceTextContainer]}>
+        <Text>{result}</Text>
+      </View>
+    )
+  }
+  return (
+    <Image style={s.diceImage} source={diceSource(result)} />
+  )
+}
+
+const makeStyles = ResponsiveStyleSheet.create(() => ({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  diceImage: {
+    margin: 30,
+    width: 100,
+    height: 100,
+  },
+  diceTextContainer: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}))
